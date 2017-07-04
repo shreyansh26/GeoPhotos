270b9c914c85fe5b2ec2298711823214e4c08129
Shreyansh <shreyansh.pettswood@gmail.com>
2017-07-04 13:06:02 +0530

    Instagram REST application with 5 second AJAX refresh

--- instaphotos/views.py
+++ instaphotos/views.py
@@ -5,6 +5,8 @@ from .forms import PostForm
 # Create your views here.
 def image_list(request):
     form = PostForm(request.POST or None)
+    url_list = []
+    url = ''
     if request.method == 'POST':
         if form.is_valid():
             location = form.cleaned_data['location']
@@ -21,11 +23,12 @@ def image_list(request):
             r2 = requests.get(insta_url, params=payload)
             jsonval2 = r2.json()
             # First two images for testing
-            url_list = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST7YDjHjnP5DURcPrZpcFPO6BI6I4kOiqTvNeCy4NRYFbA--5J',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf4ZJbn77pAKbRtt9oSbaNONm-LXB4a6TLnZzGiURXflEA5dHwfg']
+            url_list = [('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST7YDjHjnP5DURcPrZpcFPO6BI6I4kOiqTvNeCy4NRYFbA--5J', '1'),
+            ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf4ZJbn77pAKbRtt9oSbaNONm-LXB4a6TLnZzGiURXflEA5dHwfg', '2')]
             for i in jsonval2['data']:
-                url_list.append(i['images']['low_resolution']['url'])
+                url_list.append((i['images']['low_resolution']['url'], i['id']))
             print(url_list)
+            url = insta_url + '?lat=' + str(lat) + '&lng=' + str(lng) + '&access_token=' + 'ACCESS_TOKEN'
         else:
             form = PostForm()
-    return render(request, 'instaphotos/base.html', {'form': form, 'url_list': url_list})
+    return render(request, 'instaphotos/base.html', {'form': form, 'url_list': url_list, 'insta_url': url})
